d767d43a4450d2db51af699908aac90ff5d4aca7
kolodani <danielvinzia@gmail.com>
2025-02-23 20:17:32 -0300

    Python para todos, expresiones

---
+++ 000-ejemplos/002-expresiones/ejemplo002.py
@@ -0,0 +1,14 @@
+xlq3z9ocd = 35.0
+xlq3z9afd = 12.50
+xlq3p9afd = xlq3z9ocd + xlq3z9afd
+print(xlq3p9afd)
+
+a = 35.0
+b = 12.50
+c = a + b
+print(c)
+
+hours = 35.0
+rate = 12.50
+pay = hours + rate
+print(pay)
