03789e6379833814bba635ec4618fbe3886dd390
kolodani <danielvinzia@gmail.com>
2025-02-28 00:04:27 -0300

    Python para todos, Funciones

---
+++ 000-ejemplos/006-funciones/ejemplo003.py
@@ -0,0 +1,12 @@
+print(float(99) / 100)
+
+i = 42
+print(type(i))
+
+f = float(i)
+print(f)
+
+print(type(f))
+
+print(1 + 2 * float(3) / 4 - 5)
+
---
+++ 000-ejemplos/006-funciones/ejemplo004.py
@@ -0,0 +1,11 @@
+sval = '123'
+print(type(sval))
+
+# print(sval + 1)
+
+ival = int(sval)
+print(type(ival))
+print(ival + 1)
+
+nsv = 'hello bob'
+#niv = int(nsv)
