d5bae1222b3d52179c5957b531616c44f4befa1c
kolodani <danielvinzia@gmail.com>
2025-03-10 16:35:59 -0300

    Python para todos, Lists Operations

---
+++ 000-ejemplos/017-lists_operations/ejemplo005.py
@@ -0,0 +1,4 @@
+some = [1, 9, 21, 10, 16]
+print(9 in some)
+print(15 in some)
+print(20 not in some)
---
+++ 000-ejemplos/017-lists_operations/ejemplo006.py
@@ -0,0 +1,4 @@
+friends = ["Joseph", "Gleen", "Sally"]
+friends.sort()
+print(friends)
+print(friends[1])
