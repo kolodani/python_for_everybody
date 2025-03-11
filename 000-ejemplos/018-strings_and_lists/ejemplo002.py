b4760407f80f544c1c86ac349a6a7f98252e22dd
kolodani <danielvinzia@gmail.com>
2025-03-11 15:15:29 -0300

    Python para todos, Strings and Lists

---
+++ 000-ejemplos/018-strings_and_lists/ejemplo002.py
@@ -0,0 +1,12 @@
+line = "A lot              of spaces"
+etc = line.split()
+print(etc)
+line = "first;second;third"
+thing = line.split()
+print(thing)
+print(len(thing))
+thing = line.split(";")
+print(thing)
+print(len(thing))
+for w in thing:
+    print(w)
