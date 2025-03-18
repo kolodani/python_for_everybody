86e4f4b2771a1554682a37debada64547954c6ca
kolodani <danielvinzia@gmail.com>
2025-03-18 18:17:31 -0300

    Python para todos, Comparing and sorting tuples

---
+++ 000-ejemplos/023-comparing_and_sorting_tuples/ejemplo001.py
@@ -0,0 +1,3 @@
+d = {"c": 22, "a": 10, "b": 1}
+print(d.items())
+print(sorted(d.items()))
---
+++ 000-ejemplos/023-comparing_and_sorting_tuples/ejemplo002.py
@@ -0,0 +1,6 @@
+d = {"c": 22, "a": 10, "b": 1}
+t = sorted(d.items())
+print(t)
+
+for k, v in sorted(d.items()) :
+    print(k , v)
